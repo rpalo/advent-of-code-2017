fd51b5b28afd11a454c7a43bdbbee892703a2c59
Ryan Palo <ryan@thepalos.com>
2017-12-12 14:44:31 -0800

    Complete Day 12 Part 2

--- challenges/day12/pipe_graph_test.rb
+++ challenges/day12/pipe_graph_test.rb
@@ -4,7 +4,7 @@ require 'minitest/autorun'
 require_relative 'pipe_graph'
 
 class TestPipeGraph < Minitest::Test
-  def test_simple_graph
+  def setup
     connections = <<~HEREDOC
       0 <-> 2
       1 <-> 1
@@ -14,7 +14,16 @@ class TestPipeGraph < Minitest::Test
       5 <-> 6
       6 <-> 4, 5
     HEREDOC
-    g = PipeGraph.from_instructions(connections)
-    assert_equal 6, g.subgraph_with('0').size
+    @g = PipeGraph.from_instructions(connections)
+  end
+
+  # Part 1: How big is the subgraph with 0
+  def test_simple_graph
+    assert_equal 6, @g.subgraph_with('0').size
+  end
+
+  # Part 2: How many subgraphs are there?
+  def test_group_count
+    assert_equal 2, @g.subgraphs.size
   end
 end
