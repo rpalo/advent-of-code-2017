357c680de60eac3cfc7d195ea815d38f029a67f2
Ryan Palo <ryan@thepalos.com>
2017-12-13 11:20:52 -0800

    Finish day 10 part 1

--- challenges/day10/day10.rb
+++ challenges/day10/day10.rb
@@ -1,3 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'knot_hash'
+
+lengths = ARGF.read.chomp.split(',').map(&:to_i)
+knot = KnotHash.new(255)
+knot.twist(*lengths)
+puts "Result of multiplication", knot[0] * knot[1]
--- challenges/day10/knot_hash.rb
+++ challenges/day10/knot_hash.rb
@@ -1,12 +1,35 @@
 # frozen_string_literal: true
 
+# A hashing algorithm based on twisting knots in string
 class KnotHash
   def initialize(limit)
     @elements = (0..limit).to_a
+    @size = limit + 1
   end
 
   def twist(*lengths)
-    # pass
+    position = 0
+    skip_size = 0
+    flipstack = []
+    lengths.each do |length|
+      flipstack = get(position, length)
+      while item = flipstack.pop
+        @elements[position] = item
+        position = (position + 1) % @size
+      end
+      position = (position + skip_size) % @size
+      skip_size += 1
+    end
+  end
+
+  def get(start, length)
+    position = start
+    results = []
+    length.times do
+      results << @elements[position]
+      position = (position + 1) % @size
+    end
+    results
   end
 
   def [](ind)
