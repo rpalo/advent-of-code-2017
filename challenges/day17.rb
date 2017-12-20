3848894c1055cf024c63bd239695b8fd16f0d512
Ryan Palo <ryan@thepalos.com>
2017-12-19 18:48:26 -0800

    Lint

--- challenges/day17.rb
+++ challenges/day17.rb
@@ -10,7 +10,7 @@ lock = Spinlock.new
 lock.spin(step_size, 2017)
 puts "Item after 2017: #{lock[lock.current + 1]}"
 
-lock  = Spinlock.new
+lock = Spinlock.new
 lock.spin(step_size, 50_000_000)
 result = lock[1]
 puts "Item after 0 after 50,000,000: #{result}"
